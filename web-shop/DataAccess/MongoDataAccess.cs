f58fe9a64999ec49c2bb5f56137b62c2dea1ce23
baovy0105 <91138922+baovy0105@users.noreply.github.com>
2022-04-26 13:48:15 +0700

    Update MongoDataAccess.cs
    add singleton pattern for connect database

--- web-shop/DataAccess/MongoDataAccess.cs
+++ web-shop/DataAccess/MongoDataAccess.cs
@@ -1,6 +1,25 @@
-﻿namespace web_shop.DataAccess
+using MongoDB.Driver;
+namespace web_shop.DataAccess
 {
     public class MongoDataAccess
     {
+        private const string connectionString = "mongodb://localhost:27017";
+        private const string databaseName = "Shop_fashion";
+        private static MongoDataAccess _instance;
+        private MongoDataAccess() { }
+        public static MongoDataAccess getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance=new MongoDataAccess();
+            }
+            return _instance;
+        }
+        public IMongoCollection<T> CollectionToMongo<T>(in string collection)
+        {
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databaseName);
+            return db.GetCollection<T>(collection);
+        }
     }
 }
